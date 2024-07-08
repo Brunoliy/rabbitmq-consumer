937359cc194375707517d37e67449bafe15dc14e
Bruno de Oliveira <obruno.oliveira@outlook.com>
2024-07-08 16:38:17 -0300

    fix port error and add docker-compose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import pika
 import json
+import time
 
 app = FastAPI()
 
@@ -9,11 +10,18 @@ def callback(ch, method, properties, body):
     print(f" [x] Received {message}")
 
 def start_consuming():
-    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-    channel = connection.channel()
-    channel.queue_declare(queue='my_queue')
-    channel.basic_consume(queue='my_queue', on_message_callback=callback, auto_ack=True)
-    channel.start_consuming()
+    while True:
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+            channel = connection.channel()
+            channel.queue_declare(queue='my_queue')
+            channel.basic_consume(queue='my_queue', on_message_callback=callback, auto_ack=True)
+            print(" [*] Waiting for messages. To exit press CTRL+C")
+            channel.start_consuming()
+        except Exception as e:
+            print(f"Error connecting to RabbitMQ: {e}")
+            time.sleep(5)  # Espera 5 segundos antes de tentar novamente
+
 
 @app.on_event("startup")
 def startup_event():
@@ -23,4 +31,4 @@ def startup_event():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8002)
